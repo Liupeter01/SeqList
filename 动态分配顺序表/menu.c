aa1208b7b86ed15c4cf36c9818b2c7511bd09fdb
lph <liupeter012001@live.cn>
2020-11-04 09:21:52 +0800

    更改目录的说明

--- 动态分配顺序表/menu.c
+++ 动态分配顺序表/menu.c
@@ -13,7 +13,7 @@ void Menu(SeqList* list)
                     printf("*   [7]  find                 \t[8] length                   \t*\n");
                     printf("*   [9]  delete_pos      \t[10] delete_val           \t*\n");
                     printf("*   [11]  Sort               \t[12] reverse               \t*\n");
-                    printf("*   [13]  Clear             \t[14] Destroy(dangerous) \t*\n");
+                    printf("*   [13]  Clear             \t[14] Destroy(Dangerous!!) \t*\n");
                     printf("*   [15]  Cls                \t[16] ExtendSpace        \t*\n");
                     printf("*   [0]  Quit                                                 \t*\n");
                     printf("*****************************************************************\n");
